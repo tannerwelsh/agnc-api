552a06bfc29c4c9830054317a4558676e853eca3
Tanner Welsh <tannerwelsh@gmail.com>
2015-11-07 18:52:17 -0500

    Set dev and test db name in env.js

--- env.js
+++ env.js
@@ -4,4 +4,7 @@ if (!process.env.NODE_ENV)
 if (/development|test/.test(process.env.NODE_ENV))
   require('dotenv').load();
 
+if (/development|test/.test(process.env.NODE_ENV))
+  process.env.COUCHDB_NAME += '_' + process.env.NODE_ENV;
+
 module.exports = process.env;
