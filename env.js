6b67a128dec7649f4b32a14cb8c83b7db10fb9a9
Tanner Welsh <tannerwelsh@gmail.com>
2015-11-07 19:24:27 -0500

    Run production server locally on port 3333 (fix #3)

--- env.js
+++ env.js
@@ -1,8 +1,7 @@
 if (!process.env.NODE_ENV)
   process.env.NODE_ENV = 'development';
 
-if (/development|test/.test(process.env.NODE_ENV))
-  require('dotenv').load();
+require('dotenv').load();
 
 if (/development|test/.test(process.env.NODE_ENV))
   process.env.COUCHDB_NAME += '_' + process.env.NODE_ENV;
